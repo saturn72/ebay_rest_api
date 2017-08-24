5fa805680db4895aa0bed8f955525c5d9ac1a6b1
roi shabtai <roi@saturn72.com>
2017-08-24 18:06:12 +0300

    init commit

---
+++ HtmlElementStripper/HtmlElementStripper/GallerProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using HtmlAgilityPack;
+
+namespace HtmlElementStripper
+{
+    public class GallerBuilder
+    {
+        private static string _cssGalleryCode;
+
+        private static string CssGalleryCode
+        {
+            get { return _cssGalleryCode ?? (_cssGalleryCode = LoadCssGalleryCode()); }
+        }
+
+        private static string LoadCssGalleryCode()
+        {
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            var file = Path.Combine(dir, "Resources\\GalleryCss.txt");
+            return File.ReadAllText(file);
+        }
+
+        internal static HtmlNode BuildImageGalleryNode(IEnumerable<string> imageLinks)
+        {
+            var galleryImagesNode = HtmlNode.CreateNode("<header></header>");
+
+            var imageNodes = galleryImagesNode.ChildNodes;
+            foreach (var imgLink in imageLinks)
+                imageNodes.Add(HtmlNode.CreateNode(string.Format(@"<div><img src=""{0}"" alt></div>", imgLink)));
+
+            var galleryCssNode =  HtmlNode.CreateNode("<style>"+CssGalleryCode + "</style>");
+
+            var res =  HtmlNode.CreateNode("<div></div>");
+            res.ChildNodes.Add(galleryImagesNode);
+            res.ChildNodes.Add(galleryCssNode);
+
+            return res;
+        }
+    }
+}
---
+++ HtmlElementStripper/HtmlElementStripper/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿#region Usings
+
+using System.Linq;
+using System.Windows;
+using HtmlAgilityPack;
+
+#endregion
+
+namespace HtmlElementStripper
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private HtmlDocument LoadHtmlDocumentFromTextBox()
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(txtInput.Text);
+            return htmlDocument;
+        }
+
+        private void btnStripImages_Click(object sender, RoutedEventArgs e)
+        {
+            var doc = LoadHtmlDocumentFromTextBox();
+
+            var docNode = doc.DocumentNode;
+            var images = docNode.Descendants("img").Select(i=>i.Attributes.FirstOrDefault(a=>a.Name=="src").Value).ToList();
+
+            HtmlNode galleryNode;
+            if (images.Any())
+                galleryNode = GallerBuilder.BuildImageGalleryNode(images);
+        }
+
+        private void btnStripCscripts_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveNodeByNameAndUpdateTextBox("script");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var txt = txtOutput.Text;
+            if (string.IsNullOrEmpty(txt) && string.IsNullOrWhiteSpace(txt))
+                return;
+
+            Clipboard.SetText(txt);
+        }
+
+        private void RemoveNodeByNameAndUpdateTextBox(string nodeName)
+        {
+            var doc = LoadHtmlDocumentFromTextBox();
+
+            var docNode = doc.DocumentNode;
+            var xpaths = docNode.Descendants(nodeName).Select(node => node.XPath).ToList();
+
+            foreach (var xpath in xpaths)
+                docNode.SelectSingleNode(xpath).Remove();
+            txtOutput.Text = docNode.OuterHtml;
+        }
+    }
+}
